0c628cfca541e1c444cbc6c01a507f6fe66400cc
Mehrdad Salehi <mhr.salehi@gmail.com>
2021-12-08 12:32:25 +0330

    cors localhost

--- src/main/java/com/niftylimos/web/config/CorsFilter2.java
+++ src/main/java/com/niftylimos/web/config/CorsFilter2.java
@@ -20,7 +20,7 @@ public class CorsFilter2 implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "12000");
         response.setHeader("Access-Control-Allow-Headers",
